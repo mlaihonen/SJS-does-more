164b40d067e0d5dfc9a8a63cfe5d3e8f4fb4cc6e
Laihonen Mindi <a1500847@myy.haaga-helia.fi>
2016-09-27 09:56:30 +0300

    TapahtumaController ja DAO täydennetty

--- src/main/java/fi/sjs/domore/controller/TapahtumaController.java
+++ src/main/java/fi/sjs/domore/controller/TapahtumaController.java
@@ -1,5 +1,36 @@
 package fi.sjs.domore.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fi.sjs.domore.bean.Tapahtuma;
+import fi.sjs.domore.dao.TapahtumaDAO;
+
+
 public class TapahtumaController {
 	
+	@Inject
+	private TapahtumaDAO dao;
+	
+	public TapahtumaDAO getDao() {
+		return dao;
+	}
+
+	public void setDao(TapahtumaDAO dao) {
+		this.dao = dao;
+	}
+	
+	//Listaa kaikki tapahtumat
+		@RequestMapping(value="lista", method=RequestMethod.GET)
+		public String getList(Model model) {
+			List<Tapahtuma> tapahtumat = new ArrayList<Tapahtuma>(dao.haeKaikki());
+			model.addAttribute("tapahtumat", tapahtumat);
+			return "etusivu";
+		}
 }
--- src/main/java/fi/sjs/domore/dao/TapahtumaDAO.java
+++ src/main/java/fi/sjs/domore/dao/TapahtumaDAO.java
@@ -1,5 +1,33 @@
 package fi.sjs.domore.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import fi.sjs.domore.bean.Tapahtuma;
+
 public class TapahtumaDAO {
+	
+	@Inject
+	private JdbcTemplate jdbcTemplate;
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	public List<Tapahtuma> haeKaikki() {
+		String sql = "select t_id, t_nimi, t_kuvaus, t_pvm, t_aika, t_paikka from tapahtumat;";
+		//List<Tapahtuma> tapahtumat = jdbcTemplate.query(sql, mapper);
+		//return tapahtumat;
+		return null; //poista tämä
+	}
+
 
 }
