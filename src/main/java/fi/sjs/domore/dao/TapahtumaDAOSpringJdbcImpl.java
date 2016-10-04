31f7d513a925303dfdc1ff41cb3f52d741efab83
Laukkanen Suvi <a1500951@myy.haaga-helia.fi>
2016-10-04 09:13:30 +0300

    tapahtumadao muokkaus ja daoimpl luonti

---
+++ src/main/java/fi/sjs/domore/dao/TapahtumaDAOSpringJdbcImpl.java
@@ -0,0 +1,33 @@
+package fi.sjs.domore.dao;
+
+import java.util.List;
+import javax.inject.Inject;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import fi.sjs.domore.bean.Tapahtuma;
+
+@Repository
+public class TapahtumaDAOSpringJdbcImpl {
+	
+	@Inject
+	private JdbcTemplate jdbcTemplate;
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	public List<Tapahtuma> haeKaikki() {
+		String sql = "select t_id, t_nimi, t_kuvaus, t_paikka from tapahtuma;";
+		RowMapper<Tapahtuma> mapper = new TapahtumaRowMapper();
+		List<Tapahtuma> tapahtumat = jdbcTemplate.query(sql, mapper);
+		return tapahtumat;
+	}
+
+}
