e7ca8aab2a5fe5c96de42e27abd6e165930b9517
Laukkanen Suvi <a1500951@myy.haaga-helia.fi>
2016-11-03 10:43:53 +0200

    ja vielä tapahtumadaoa

--- src/main/java/fi/sjs/domore/dao/TapahtumaDAOHibernateImpl.java
+++ src/main/java/fi/sjs/domore/dao/TapahtumaDAOHibernateImpl.java
@@ -8,7 +8,6 @@ import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import fi.sjs.domore.bean.Kayttaja;
 import fi.sjs.domore.bean.Tapahtuma;
 
 @Transactional
