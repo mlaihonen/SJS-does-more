95d9a56d2381e2c80c8c8fdd55a33df88e6cdab7
Laukkanen Suvi <a1500951@myy.haaga-helia.fi>
2016-11-03 10:42:02 +0200

    tapahtumacontrolleriin lisaauusi metidi

--- src/main/java/fi/sjs/domore/dao/TapahtumaDAOHibernateImpl.java
+++ src/main/java/fi/sjs/domore/dao/TapahtumaDAOHibernateImpl.java
@@ -8,6 +8,7 @@ import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import fi.sjs.domore.bean.Kayttaja;
 import fi.sjs.domore.bean.Tapahtuma;
 
 @Transactional
@@ -17,6 +18,9 @@ public class TapahtumaDAOHibernateImpl implements TapahtumaDAO{
 	@PersistenceContext
 	private EntityManager em;
 	
+	public void lisaaUusi(Tapahtuma tapahtuma) {
+        em.persist(tapahtuma);
+    }
 	
 	public List<Tapahtuma> haeKaikki() {
 		return em.createQuery("select tapahtuma from TapahtumaImpl tapahtuma", Tapahtuma.class).getResultList();
